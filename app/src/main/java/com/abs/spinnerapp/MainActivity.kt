c243e2f1ec3b3e629380eb9e2c6f93849e02b1a5
AbdoATorky <abdalsalamtorky@gmail.com>
2026-03-15 20:56:52 +0200

    Initialize Spinner App project
    - Add an `anime_list` string array to resources for the spinner's data source.
    - Configure UI layout with a `ConstraintLayout` containing an `ImageView` background, a `TextView` label, and a `Spinner`.

---
+++ app/src/main/java/com/abs/spinnerapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.abs.spinnerapp
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+
+        val spinner = findViewById<Spinner>(R.id.anime_list)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.anime_list,
+            android.R.layout.simple_spinner_item
+        ).also {adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter =adapter
+        }
+
+    }
+}
